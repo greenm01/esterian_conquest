c63e82ce181f43bd8e662f8b27d15b5066ace87f
Mason Green <mason@greenm01.net>
2026-03-21 14:25:00 -0400

    repo: ignore local analysis clutter

--- .ghidra/tmp/MyScript.java
+++
@@ -1,7 +0,0 @@
-import ghidra.app.script.GhidraScript;
-public class MyScript extends GhidraScript {
-    @Override
-    protected void run() throws Exception {
-        println("It worked!");
-    }
-}
