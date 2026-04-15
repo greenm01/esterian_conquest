b61f3762a2b51421186100bd2afb1d9adad5b094
Mason Green <mason@greenm01.net>
2026-04-15 11:28:54 -0400

    improved error reporting for nc-dash by wrapping the main entry point to catch and log fatal errors.

--- rust/nc-dash/src/main.rs
+++ rust/nc-dash/src/main.rs
@@ -1,3 +1,6 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    nc_dash::main_entry()
+fn main() {
+    if let Err(err) = nc_dash::main_entry() {
+        eprintln!("nc-dash fatal error: {err}");
+        std::process::exit(1);
+    }
 }
